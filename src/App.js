94024687260ba27c3ed24275ad57d62d7001a57a
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-08 21:25:12 +0100

    make button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="App-header">
+        hello
+        <a href="#" className="btn btn-primary"></a>
+      </header>
     </div>
   );
 }
