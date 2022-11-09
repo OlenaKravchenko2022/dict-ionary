8aba44ce90ac0f28e1a8ef78f9d9b5e2db8194bb
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-09 17:36:53 +0100

    search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello
-        <a href="#" className="btn btn-primary"></a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          hello
+          <a href="#" className="btn btn-primary"></a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Olena Kravchenko</footer>
+      </div>
     </div>
   );
 }
